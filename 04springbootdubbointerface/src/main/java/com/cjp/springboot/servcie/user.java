b2a4cc48fd1bde399e9cc9cf7c00cfbcede0a636
13_chengjinpeng <cjp1036934016@163.com>
2019-03-13 09:31:27 +0800

    完善接口模块

--- 04springbootdubbointerface/src/main/java/com/cjp/springboot/servcie/user.java
+++ 04springbootdubbointerface/src/main/java/com/cjp/springboot/servcie/user.java
@@ -1,6 +1,10 @@
 package com.cjp.springboot.servcie;
 
 
+import com.cjp.springboot.model.User;
+
 public interface user {
     public String SayHello(String name);
+
+    public User getUser(int id);
 }
