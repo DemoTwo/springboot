c0d889898d2268385b7df023806d3392481500ff
13_chengjinpeng <cjp1036934016@163.com>
2019-03-13 09:08:50 +0800

    增加dubbo的接口模块

---
+++ 04springbootdubbointerface/src/main/java/com/cjp/springboot/model/User.java
@@ -0,0 +1,31 @@
+package com.cjp.springboot.model;
+
+public class User {
+    private int id;
+    private  String name;
+    private  String phone;
+// alt +insert调用方法 自动生成get set方法
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ 04springbootdubbointerface/src/main/java/com/cjp/springboot/servcie/user.java
@@ -0,0 +1,6 @@
+package com.cjp.springboot.servcie;
+
+
+public interface user {
+    public String SayHello(String name);
+}
